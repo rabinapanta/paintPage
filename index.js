f0918c984d8501ce103f7c147aa4ac29590519c9
rabinapanta <rabinapanta999@gmail.com>
2024-04-13 03:14:59 -0400

    my commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('main');
-const ctx = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
@@ -7,10 +7,27 @@ let hue = 0;
 let direction = true;
 let brushColor = 'black';
 
+function draw(e) {
+    if (!isDrawing) return;
+    context.strokeStyle = brushColor === 'eraser' ? 'white' : brushColor;
+    context.lineWidth = document.getElementById('slider').value;
+    context.lineJoin = 'round';
+    context.lineCap = 'round';
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    if (brushColor === 'eraser') {
+        context.globalCompositeOperation = 'destination-out';
+    } else {
+        context.globalCompositeOperation = 'source-over';
+    }
+}
 
 
 document.querySelectorAll('.btn-color').forEach(button => {
     button.addEventListener('click', () => {
-      brushColor = button.id;
+        brushColor = button.id;
     });
-  });
+});
